82e5fdb259a8ae332fe5b5da533e35bf374fa4d4
ovr58 <106969823+ovr58@users.noreply.github.com>
2023-10-26 23:11:06 +0500

    site understands what language to use

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -14,10 +14,23 @@ import {
   Works,
   StarsCanvas,
 } from './components';
+import { useEffect } from 'react';
 
 function Page() {
+  
   const [lang, setLang] = useState('en');
+  
+  useEffect(() => {
+    
+    const browser_lang = window.navigator.language.substring(0,2);
+    
+    browser_lang == 'ru' || browser_lang == 'ka' || browser_lang == 'en' ? setLang(browser_lang) : setLang('en')
+    
+  }, []);
+  
   const value = useMemo(() => ({ lang, setLang }), [lang]);
+
+  console.log(value)
   return (
     <SiteLang.Provider value={value}>
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
